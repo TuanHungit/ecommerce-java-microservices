541306b4fe4334f5abeedf2ba74936dcddc96b4c
hungnt <tuanhungit14@gmail.com>
2022-07-10 14:16:07 +0700

    add jenkins server

---
+++ jenkins/createJobs.groovy
@@ -0,0 +1,55 @@
+pipelineJob('pipelineJob') {
+    definition {
+        cps {
+            script(readFileFromWorkspace('pipelineJob.groovy'))
+            sandbox()
+        }
+    }
+}
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/TuanHungit/spring-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/TuanHungit/spring-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
+
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/TuanHungit/spring-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
